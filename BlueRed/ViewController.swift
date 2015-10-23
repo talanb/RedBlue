7c520391ba08cffc6e114e3d93aa76f00e6a427e
Todd Breiholz <talanb@gmail.com>
2015-10-23 15:33:04 -0500

    Initial Commit

---
+++ BlueRed/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BlueRed
+//
+//  Created by Todd Breiholz on 10/23/15.
+//  Copyright © 2015 Mello Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
